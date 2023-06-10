fdfd24703f4837521ea22a61a3e174670936d3b2
fatih ozer <fatihozer246@hotmail.com>
2023-06-10 05:12:56 +0200

    updated09June

--- MagicVilla/Program.cs
+++ MagicVilla/Program.cs
@@ -1,3 +1,7 @@
+using MagicVilla.Data;
+using MagicVilla.Models;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,7 +27,67 @@ app.MapGet("/helloworldexception", () =>
 {
 	return Results.NotFound("sorry guys, we couldn't find your webpage, come tomorrow");
 });
-app.MapPost("/helloworldpost",()=> "Hello World from API Post"); // ==> HTTP POST
+
+app.MapGet("/helloworldwithid/{id:int}", (int id) =>
+{
+	return Results.Ok("your id is " + id);
+}); // id would be int only, if we write a string it gives 404 not found, but if couldn't put 'id:int' there, and then give a string value, it gives 400 bad request.
+
+app.MapPost("/helloworldpost", () => Results.Ok("Hello World from API Post")); // ==> HTTP POST
+
+
+/*
+	After this line, we make a simple crud operations using real world sample
+	We created coupon class and couponstore data class
+ */
+
+// app.MapGet("/api/coupon", () => Results.Ok(CouponStore.couponList)); // write this a bit longer
+app.MapGet("/api/coupon", (ILogger<Program> _logger) =>
+{
+	_logger.Log(LogLevel.Information, "Getting all coupons"); // used ILogger as dependency injection
+	return Results.Ok(CouponStore.couponList);
+}).WithName("GetCoupons").Produces<IEnumerable<Coupon>>(200);
+
+app.MapGet("/api/coupon/{id:int}", (int id) =>
+{
+	return Results.Ok(CouponStore.couponList.FirstOrDefault(coupon => coupon.Id == id));
+}).WithName("GetCoupon").Produces<Coupon>(200);
+
+app.MapPost("/api/coupon", ([FromBody] Coupon coupon) =>
+{
+	if (coupon.Id != 0 || string.IsNullOrEmpty(coupon.Name))
+		return Results.BadRequest("Invalid ID or coupon name");
+
+	// use another (name) validation to not to create the same coupon again
+	if (CouponStore.couponList.FirstOrDefault(u => u.Name.ToLower() == coupon.Name.ToLower()) != null)
+	{
+		return Results.BadRequest("Coupon Name already exists.");
+	}
+
+	coupon.Id = CouponStore.couponList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+	CouponStore.couponList.Add(coupon);
+
+	// when we create, the coupon ID must be 0, if it is not, then that is not a valid ID.
+	// because ID is something that typically the database, all the code is responsible for adding.
+	// but here we don't have/use any database. So, we need to populate the ID
+	// CouponStore.couponList.OrderByDescending(u=>u.Id).FirstOrDefault().Id ==> this gives the last id number
+
+	// return Results.Ok(coupon);
+	// sometimes we use 'created' rather than returning an Ok in HTTP POST
+	//return Results.Created($"/api/coupon/{coupon.Id}", coupon);
+	return Results.CreatedAtRoute("GetCoupon", new {id=coupon.Id},coupon);
+
+}).WithName("CreateCoupon").Accepts<Coupon>("application/json").Produces<Coupon>(201).Produces(400);
+
+app.MapPut("/api/coupon", () =>
+{
+
+});
+
+app.MapDelete("/api/coupon/{id:int}", (int id) =>
+{
+
+});
 
 app.UseHttpsRedirection();
 app.Run();
